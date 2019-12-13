c4157180806999c8307c4992b97468787ad8c5ac
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-13 16:32:20 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
+[ ]  
 end
 
 def create_an_array
-  
+countries= ["italy", "japan", "usa", "russia"]  
 end
 
 def add_element_to_end_of_array(array, element)
-  
+countries= ["italy", "japan", "usa", "russia"]
+countries<< "arrays!"  
 end
 
 def add_element_to_start_of_array(array, element)
-  
+countries= ["italy", "japan", "usa", "russia"]
+countries.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+ countries= ["italy", "japan", "usa", "russia"]
+countries<< "arrays!" 
+ countries.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  
+ countries= ["italy", "japan", "usa", "russia"]
+countries.unshift "wow"
+countries.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  countries= ["italy", "japan", "am","usa", "russia"]
+ countries[2] 
 end
 
 def retrieve_first_element_from_array(array)
-  
+countries= ["wow", "japan", "italy"]
+countries [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  countries= ["italy", "japan", "usa", "russia"]
+countries<< "arrays!" 
+countries [-1]
 end
